b30feda2f3055d8d92443ca7e87d8afa0322a73b
Emma Janani <99275087+EmmaJanani13@users.noreply.github.com>
2022-05-04 23:44:17 +0000

    Finished web page

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Created by: Emma Janani
+// Created on: April 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+
+ function displayCost() {
+  // declare variables
+  let cost = "Please make a selection above."
+   
+	// get user input
+	let age = parseInt(document.getElementById('age').value)
+  let select = document.getElementById('day');
+	let day = select.options[select.selectedIndex].value;
+  
+  if (age < 5 || age > 95) {
+		cost = "The cost is FREE for you."
+	}
+	else if ((day == "Tuesday") || (day == "Thursday)") 
+			 || (age >=12) && (age <=21)) {
+		cost = "You get a student discount."
+	}
+	else if ((age > 0) || (day != "")) {		
+		cost = "You have to pay regular price."
+	}
+	
+  	// display the greeting
+  	document.getElementById('display-results').innerHTML = cost
+}
+   
